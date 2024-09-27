510d4e3aff891a4a5ab9e53bfa3dbaf84ff8e3ce
whysumedh <ksaisumedh@gmail.com>
2024-09-27 12:03:35 +0530

    date changes

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with st.expander("Disclaimer", expanded=True):
     - It may not be accurate or up-to-date (Will try to update as new selections keep coming).
     - The CTC information is not available for most of the Summer PPOs, and Internship Offers.
     - Only 21 Batch B.Tech details have been considered.
-    - Data Updated as on 28 September 2024.
+    - Data Updated as on 27 September 2024.
     """)
 
 
