549656488be21e2de542e75bf0de7a786968cce8
whysumedh <ksaisumedh@gmail.com>
2024-09-27 22:18:40 +0530

    reddit post link

--- app.py
+++ app.py
@@ -170,4 +170,12 @@ st.markdown("""
         </a>
     </p>
     """, unsafe_allow_html=True)
+st.markdown("""
+    <p style='text-align: center;'>
+        Check out the Reddit post!!: 
+        <a href='https://forms.gle/ZVSaNbbFHRjwevEN6' target='_blank'>
+            Reddit Post
+        </a>
+    </p>
+    """, unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Made by Sumedh K</p>", unsafe_allow_html=True)
