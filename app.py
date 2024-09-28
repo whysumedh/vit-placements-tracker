28023fd07b9cf361ab8376ad2bb66af6e282d5db
whysumedh <ksaisumedh@gmail.com>
2024-09-28 11:49:11 +0530

    sidebar redacted , data update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with st.expander("Disclaimer", expanded=True):
                 (Please provide PPO CTC Info in the below gform if yk for the respective company)**
     - **The average,median CTC stats have been considered from available CTC information only.**
     - Only 21 Batch B.Tech details have been considered.
-    - Data Updated as on 28 September 2024 9:59AM.
+    - Data Updated as on **28 September 2024 11:48AM**.
     """)
 
 
@@ -154,11 +154,11 @@ with tab3:
     st.write("**List of Companies:**")
     st.table(company_df)
 st.sidebar.write("**Recent Changes**")
-st.sidebar.markdown("""
-    - VISA PPO CTC Updated to 32LPA
-    - Fidelity PPO CTC Updated to 15LPA
-    - Microsoft PPO Updated to 55LPA
-""")
+# st.sidebar.markdown("""
+#     - VISA PPO CTC Updated to 32LPA
+#     - Fidelity PPO CTC Updated to 15LPA
+#     - Microsoft PPO Updated to 55LPA
+# """)
 
 st.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
 st.markdown("""
