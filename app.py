85dbc21cf4997217560448cad2801f0b3f322a83
whysumedh <ksaisumedh@gmail.com>
2024-10-10 11:30:43 +0530

    sidebar add

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ with tab3:
 
 
 
-# st.sidebar.write("Deloitte USI made 88 offers, 84(B.Tech)+4(M.Tech)")
+st.sidebar.write("Oracle CTC Updated Dev-19.3LPA and Consulting - 9.34LPA")
 # st.sidebar.write("IBM PPO Updated to 12LPA")
 # st.sidebar.write("Sabre CTC Info Updated SA-16.84 and BA-16.29")
 # st.sidebar.write(""" Thanks for the responses, the following CTC info is updated.""")
