9a38661172293ca2dbcb853242f147e2ac762f2a
Sumedh <ksaisumedh@gmail.com>
2024-11-30 17:35:41 +0530

    favicon removed

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def convert_ctc_to_numeric(ctc):
 
 df['CTC'] = df['CTC'].apply(convert_ctc_to_numeric)
 
-st.set_page_config(page_title="VIT Placements" ,page_icon="favicon-16x16.png",layout="wide")
+st.set_page_config(page_title="VIT Placements" ,layout="wide")
 
 st.title("VIT Placements 2024-2025")
 
