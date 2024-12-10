3342e41022eb5f68308a85650cd20e019ce083ed
Sumedh <ksaisumedh@gmail.com>
2024-12-10 18:22:30 +0530

    cognizant 6.75 update

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ else:
     formatted_date = "No commits found."
 
 df=pd.read_excel('google_sheet_data.xlsx')
-wdf=pd.read_excel('WITCH-P-1.xlsx')
+wdf=pd.read_excel('WITCH-P-2.xlsx')
 def convert_ctc_to_numeric(ctc):
     try:
         return float(ctc.replace('LPA', '').strip())
