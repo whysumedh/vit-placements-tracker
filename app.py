7d5b05f33cc56c92afe7f997d625475e313c5983
Sumedh <ksaisumedh@gmail.com>
2025-08-12 17:41:17 +0530

    text lien hcn

--- app.py
+++ app.py
@@ -101,7 +101,6 @@ with st.expander("Disclaimer"):
 st.write("**Note**: Please Note Only 21 Batch **B.Tech** details have been considered.")
 st.write(f"Data Updated as on **{formatted_date}.**")
 st.write("**Next Update will be the last.**")
-st.write("**Companies Monthly(Timeline) Data Updated**")
 
 dop = st.radio("Select DataFrame :", ("Combine WITCH and Normal Offers (Gender Stats Will Be Redacted)", "Consider Only Normal Offers" ))
 if dop == "Combine WITCH and Normal Offers (Gender Stats Will Be Redacted)":
